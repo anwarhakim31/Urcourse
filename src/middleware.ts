1d865dacc239af0b9280aba1189a7c34fc4ef801
anwarhakim31 <anwarhakim31@gmail.com>
2025-01-12 14:47:10 +0700

    feat(payment):try to connect webhook xendit

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,7 @@ import { NextResponse } from "next/server";
 import { withAuth } from "./middlewares/withAuth";
 
 function middleware() {
-  const response = NextResponse.next();
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-  return;
+  return NextResponse.next();
 }
 
 export default withAuth(middleware, ["admin", "profile"]);
